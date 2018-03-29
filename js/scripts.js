5a73e03e0e237cab64e36189f52d948f73c7aed5
Luke Stockburger and William Chu <luke@>
2018-03-29 10:55:39 -0700

    add min-max values on user input, js comments explaining code

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function convertRoman(array) {
   var tensArray = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
   var hundredsArray = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
   var thousandsArray = ['M', 'MM', 'MMM'];
-  // Looks up value in array from ones position and replaces it with the corresponding roman numeral array item
+  // Looks up value in array from ones position and replaces it with the corresponding roman numeral array item, ignores 0 and other irrelevant values as there is no corresponding array object
   array[0] = onesArray[(parseInt(array[0])-1)];
   array[1] = tensArray[(parseInt(array[1])-1)];
   array[2] = hundredsArray[(parseInt(array[2])-1)];
