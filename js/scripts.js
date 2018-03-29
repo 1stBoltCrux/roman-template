0efb2f6387ab59242b1fe9318b95a4d8c2a78b40
Luke Stockburger and William Chu <william@>
2018-03-29 10:03:05 -0700

    add JS to convert single digit number to Roman numeral

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
 // Business Logic
+function convertRoman(array) {
+  var onesArray = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+  var tensArray = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+  var hundredsArray = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+  var thousandsArray = ['M', 'MM', 'MMM'];
+
+  for (var i = array.length-1; i >= 0; i--) {
+    array[array.length-1] = onesArray[parseInt(array[array.length-1])-1];
+  }
+
+
+ return array;
+}
 
 // User Logic
 $(document).ready(function(){
   $("#user-input-form").submit(function(event) {
     event.preventDefault();
 
-    var userInput = parseInt($("#user-input").val());
+    var userInput = $("#user-input").val();
+    var numbersArray = userInput.split("");
 
-    $("#output").append(userInput);
+    $("#output").empty().append(convertRoman(numbersArray));
   });
 });
