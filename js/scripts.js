68fc27ea509e776092f4e40dda1ee068540e06d9
Luke Stockburger and William Chu <william@>
2018-03-29 10:38:43 -0700

    add notes to JS

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,24 @@ function convertRoman(array) {
   var tensArray = ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
   var hundredsArray = ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
   var thousandsArray = ['M', 'MM', 'MMM'];
-
+  // Looks up value in array from ones position and replaces it with the corresponding roman numeral array item
   array[0] = onesArray[(parseInt(array[0])-1)];
   array[1] = tensArray[(parseInt(array[1])-1)];
   array[2] = hundredsArray[(parseInt(array[2])-1)];
   array[3] = thousandsArray[(parseInt(array[3])-1)];
 
+  // Reverse updated array and return
   return array.reverse();
-
 }
 
 // User Logic
 $(document).ready(function(){
   $("#user-input-form").submit(function(event) {
     event.preventDefault();
-
+    // Take userInput string, convert to array and reverse
     var userInput = $("#user-input").val();
     var numbersArray = userInput.split("").reverse();
-
+    // Calls convertRoman function and passes numbersArray as argument
     $("#output").empty().append(convertRoman(numbersArray));
   });
 });
