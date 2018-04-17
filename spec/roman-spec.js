9b6af13fc9b35befa6eb8b86fa7113f8df3d2f9f
Ted Leary and William Chu <ted@>
2018-04-17 11:15:16 -0700

    add test for 3 and 4 digit number input

--- spec/roman-spec.js
+++ spec/roman-spec.js
@@ -8,9 +8,23 @@ describe('convertRoman', function() {
   });
 });
 
-  describe('convertRoman', function() {
-    it('should convert a double digit number to Roman numerals', function() {
-      var test2 = convertRoman([2,1]);
-      expect(test2).toEqual(["X","II"]);
+describe('convertRoman', function() {
+  it('should convert a double digit number to Roman numerals', function() {
+    var test2 = convertRoman([2,1]);
+    expect(test2).toEqual(["X","II"]);
+  });
+});
+
+describe('convertRoman', function() {
+  it('should convert a triple digit number to Roman numerals', function() {
+    var test2 = convertRoman([3,2,1]);
+    expect(test2).toEqual(["C","XX","III"]);
+  });
+});
+
+describe('convertRoman', function() {
+  it('should convert a four digit number to Roman numerals', function() {
+    var test2 = convertRoman([3,3,2,1]);
+    expect(test2).toEqual(["M","CC","XXX","III"]);
   });
 });
